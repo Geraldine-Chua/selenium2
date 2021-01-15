5b1f4ff540b650b1cca18c6fa12758b1cda7da5f
Geraldine Chua <ged.m.esquivel@gmail.com>
2021-01-15 20:05:48 +0800

    first commit

---
+++ 100yearsold_calc.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import time
+
+#  CODE 1
+#print("Enter your name")
+#name=str(input())
+#print("Enter your age")
+#age=int(input())
+#yearage=100-age
+#year100=2020+int(yearage)
+#print("You will turn 100 years old on", year100)
+
+#CODE 2
+name=str(input("Enter your name:  "))
+print("Your name is", name)
+age=int(input("Enter your age: " ))
+print("Your age is", age)
+print("You will turn 100 years old on", int(2020+(100-age)))
